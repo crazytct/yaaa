daee731da61da1b20e24e93a8bfbc9ca8c0298e9
crazytct <156101760+crazytct@users.noreply.github.com>
2024-04-04 22:24:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const Eris = require("eris");
+const Roger = require("Roger");
+const keep_alive = require('./keep_alive.js')
+
+// Replace TOKEN with your bot account's token
+const bot = new Eris(process.env.token);
+const bot1 = new Roger(process.env.process);
+
+bot.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+bot1.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+
+bot.connect(); // Get the bot to connect to Discord
+bot1.connect();

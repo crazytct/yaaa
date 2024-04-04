5bcb2af3ffcb714d3f2abd21405df9691a83b033
crazytct <156101760+crazytct@users.noreply.github.com>
2024-04-04 22:30:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Eris = require("eris");
-const Roger = require("roger");
+
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
-const bot1 = new Roger(process.env.process);
+const bot1 = new Eris(process.env.process);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger

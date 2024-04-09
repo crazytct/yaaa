20d2dd51e83e48b691e4a3b2ecb268cf47659309
crazytct <156101760+crazytct@users.noreply.github.com>
2024-04-09 19:20:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
-const Eris = require("eris");
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+require('dotenv').config();
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-const keep_alive = require('./keep_alive.js')
-
-// Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
-const bot1 = new Eris(process.env.process);
-
-bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+const client1 = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
 });
-bot1.on("error", (err) => {
-  console.error(err); // or your preferred logger
+const client2 = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
+});
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('YaY Your Bot Status Changed✨');
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
+  console.log(`🔗 Powered By RTX`);
 });
 
-bot.connect(); // Get the bot to connect to Discord
-bot1.connect();
+const statusMessagesBot1 = ["Listening to Spotify"];
+const statusMessagesBot2 = ["Playing Minecraft"];
+
+let currentIndexBot1 = 0;
+let currentIndexBot2 = 0;
+
+
+async function login(client) {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log(`\x1b[36m%s\x1b[0m`, `|   🐇 Logged in as ${client.user.tag}`);
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+function updateStatusAndSendMessages(client, statusMessages, currentIndex) {
+  const currentStatus = statusMessages[currentIndex];
+  const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
+
+  client.user.setPresence({
+    activities: [{ name: currentStatus, type: ActivityType.Custom }],
+    status: 'dnd',
+  });
+  return (currentIndex + 1) % statusMessages.length;
+}
+
+client1.once('ready', () => {
+  console.log(`✅ Bot 1 is ready as ${client1.user.tag}`);
+  setInterval(() => {
+    currentIndexBot1 = updateStatusAndSendMessages(client1, statusMessagesBot1, currentIndexBot1);
+  }, 10000);
+});
+
+client2.once('ready', () => {
+  console.log(`Bot 2 is ready as ${client2.user.tag}`);
+  setInterval(() => {
+    currentIndexBot2 = updateStatusAndSendMessages(client2, statusMessagesBot2, currentIndexBot2);
+  }, 10000);
+});
+
+login(client1);
+login(client2);
